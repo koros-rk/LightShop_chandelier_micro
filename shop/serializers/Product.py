1fefdf98177eae2a29f84d97f9f47701ddf3b1a1
koros <korosov.j@gmail.com>
2023-06-07 23:22:35 +0300

    product create done

--- shop/serializers/Product.py
+++ shop/serializers/Product.py
@@ -1,6 +1,8 @@
+from django.db.models import Q
 from rest_framework import serializers
 from ..models import *
 from . import BracingSerializer, CategorySerializer, ColorSerializer, ColorAvailabilitySerializer
+from rest_framework.response import Response
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -15,3 +17,43 @@ class ProductSerializer(serializers.ModelSerializer):
                   'potency', 'is_remote', 'is_led',
                   'category', 'bracing', 'colors', 'time_created',
                   'time_updated', 'show', 'availability')
+
+    def create(self, validated_data):
+        colors_data = validated_data.pop('coloravailability_set')
+        category_data = validated_data.pop('category')
+        bracing_data = validated_data.pop('bracing')
+
+        product = Product.objects.create(
+            title=validated_data['title'],
+            description=validated_data['description'],
+            main_photo=validated_data['main_photo'],
+            base_price=validated_data['base_price'],
+            plafod_count=validated_data['plafod_count'],
+            potency=validated_data['potency'],
+            is_remote=validated_data['is_remote'],
+            is_led=validated_data['is_led'],
+            show=validated_data['show'],
+            availability=validated_data['availability'],
+            bracing=Bracing.objects.get(**bracing_data),
+            category=Category.objects.get(**category_data)
+        )
+
+        # category = Category.objects.create(**category_data)
+        # product.category = category
+        #
+        # bracing = Bracing.objects.create(**bracing_data)
+        # product.bracing = bracing
+
+        for color_data in colors_data:
+            color = Color.objects.get_or_create(**color_data['color'])[0]
+            ColorAvailability.objects.create(product=product,
+                                             color=color,
+                                             main_photo=color_data['main_photo'],
+                                             availability=color_data['availability'])
+
+        product.save()
+
+        return product
+
+    def delete(self, instance):
+        instance.delete()
--- shop/viewsets/Product.py
+++ shop/viewsets/Product.py
@@ -1,4 +1,7 @@
+from django.db.models import Q
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from ..models import Product
 from ..serializers import ProductSerializer
@@ -11,3 +14,30 @@ class ProductViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = Product.objects.all().filter(show=True)
         return queryset
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        product_obj = Product.objects.filter(Q(category__title=instance.category.title)).exclude(pk=instance.pk).order_by('?')[:6]
+        related = []
+        for k in product_obj:
+            ser = self.get_serializer(k)
+            related.append(ser.data)
+        related_data = serializer.data
+        related_data["related"] = related
+        return Response(related_data)
+
+    @action(detail=False, methods=['GET'], url_path='slug/(?P<slug>[^/.]+)')
+    def get_by_slug(self, request, slug=None):
+        product = self.get_queryset().filter(slug=slug)
+        serializer = self.get_serializer(product)
+
+        product_obj = Product.objects.filter(Q(category__product__title__contains=product.category.title)).exclude(pk=product.pk).order_by('?')[:6]
+        related = []
+        for k in product_obj:
+            ser = self.get_serializer(k)
+            related.append(ser.data)
+        related_data = serializer.data
+        related_data["related"] = related
+
+        return Response(related_data)
